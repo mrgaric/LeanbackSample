6cccd209672d47e61050d20fed4452e76a91e9d1
mrgaric <mr.igordubrovin@gmail.com>
2019-09-30 13:14:21 +0300

    added default title view

--- app/src/main/java/i/d/leanback_sample/MainFragment.kt
+++ app/src/main/java/i/d/leanback_sample/MainFragment.kt
@@ -1,6 +1,33 @@
 package i.d.leanback_sample
 
-import androidx.fragment.app.Fragment
+import android.os.Bundle
+import androidx.leanback.app.BrowseSupportFragment
+import androidx.leanback.widget.SearchOrbView
+import i.d.leanback_sample.extensions.getColorRes
+import i.d.leanback_sample.extensions.getDrawableRes
 
-class MainFragment : Fragment() {
+class MainFragment : BrowseSupportFragment() {
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        initView(savedInstanceState)
+    }
+
+    private fun initView(savedInstanceState: Bundle?) {
+        context?.let { context ->
+            title = "Finch"
+            badgeDrawable = context.getDrawableRes(R.drawable.app_icon_your_company)
+            searchAffordanceColor = context.getColorRes(R.color.search_opaque)
+            searchAffordanceColors = SearchOrbView.Colors(
+                context.getColorRes(R.color.search_opaque),
+                context.getColorRes(R.color.search_opaque_bright),
+                context.getColorRes(R.color.search_opaque_icon)
+            )
+        }
+
+        setOnSearchClickedListener {
+
+        }
+    }
+
 }
